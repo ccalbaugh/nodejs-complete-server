89bbbcd5d8adcc06f3de699d0f6ad7071ef53c8c
Caden Albaugh <cadenalbaugh@gmail.com>
2019-01-14 17:11:15 -0600

    feat: add request listening logic

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const rqListener = (req, res) => {
-  console.log(req);
+  const { method, url } = req;
+
+  if (url === "/") {
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 };
 
 const server = http.createServer(rqListener);
